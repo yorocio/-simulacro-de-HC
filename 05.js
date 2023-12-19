8e1fd04e53a5ab685440077b20220b02e0e8d86b
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-12-19 17:09:44 -0300

    metodo map

--- 05.js
+++ 05.js
@@ -14,7 +14,9 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
-
+  return array.map(function(elemento){
+    return elemento[propiedad];
+  });
 }
 
 // No modifiques nada debajo de esta linea //
