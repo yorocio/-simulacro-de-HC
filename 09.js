326044fe79b9d8e270b3e53bbc2e53d45b9e7bb0
Rocio Diaz Anriquez <rocioanriquez23@gmail.com>
2023-12-29 11:28:11 -0300

    metodo filtrar

--- 09.js
+++ 09.js
@@ -24,7 +24,15 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+Array.prototype.filtrar = function (callback){
+  let arrayResultado = [];
+  for (let i = 0; i<this.length; i++){
+    if (callback(this[i])){
+      arrayResultado.push(this[i]);
+    }
+  }
+return arrayResultado;
+}
 };
 
 // No modifiques nada debajo de esta linea //
